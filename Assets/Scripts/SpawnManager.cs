c0e7b3f0b796f8f4f6bcb7fa5024dbee3b5d7b6e
duncwilliams <47325619+duncwilliams@users.noreply.github.com>
2026-01-04 20:40:07 -0500

    replaced spawning road lines with actual scrolling road texture

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstacles;
-    public GameObject roadLine;
 
     private float speedIncrement = 0.12f;
     private float spawnDelay = 1f;
@@ -15,16 +14,13 @@ public class SpawnManager : MonoBehaviour
         spawnPoint = GameObject.Find("Spawn Point").transform.position;
 
         InvokeRepeating("SpawnObstacles", spawnDelay, spawnDelay);
-        InvokeRepeating("SpawnRoadLines", 0f, spawnDelay);
     }
 
     public void SpeedUp()
     {
         CancelInvoke("SpawnObstacles");
-        CancelInvoke("SpawnRoadLines");
         spawnDelay -= speedIncrement;
         InvokeRepeating("SpawnObstacles", spawnDelay, spawnDelay);
-        InvokeRepeating("SpawnRoadLines", 0.2f, spawnDelay);
     }
 
     public void StopSpawning()
@@ -42,11 +38,5 @@ public class SpawnManager : MonoBehaviour
         float randomYRotation = Random.Range(0f, 360f);
 
         Instantiate(obstacle, spawnLocation, Quaternion.Euler(obstacle.transform.rotation.x, randomYRotation, obstacle.transform.rotation.z));
-    }
-
-    private void SpawnRoadLines()
-    {
-        Vector3 spawnLocation = new Vector3(spawnPoint.x, 0.05f, spawnPoint.z);
-        Instantiate(roadLine, spawnLocation, roadLine.transform.rotation);
     }
 }
