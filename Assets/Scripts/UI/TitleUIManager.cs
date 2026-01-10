bd5341d64e9ebe703578f6abdb9395f4a93ac393
duncwilliams <47325619+duncwilliams@users.noreply.github.com>
2026-01-09 21:35:57 -0500

    credits and clean up

--- Assets/Scripts/UI/TitleUIManager.cs
+++ Assets/Scripts/UI/TitleUIManager.cs
@@ -5,7 +5,12 @@ using UnityEngine.UI;
 public class TitleUIManager : MonoBehaviour
 {
     private Button startButton;
+    private Button creditsButton;
     private Button exitButton;
+    private Button closeButton;
+
+    public  GameObject creditsPanel;
+
     public LevelLoader levelLoader;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,16 +23,28 @@ public class TitleUIManager : MonoBehaviour
     {
         // Initialize buttons
         startButton = GameObject.Find("Start Button").GetComponent<Button>();
+        creditsButton = GameObject.Find("Credits Button").GetComponent<Button>();
         exitButton = GameObject.Find("Exit Button").GetComponent<Button>();
+        closeButton = GameObject.Find("Close Button").GetComponent<Button>();
 
         // Add listeners to buttons
         startButton.onClick.AddListener(StartGame);
+        creditsButton.onClick.AddListener(Credits);
         exitButton.onClick.AddListener(ExitGame);
+        closeButton.onClick.AddListener(ClosePanel);
+
+        // Hide credits panel
+        creditsPanel.SetActive(false);
     }
 
     private void StartGame()
     {
         levelLoader.LoadLevel("Main");
+    }
+
+    private void Credits()
+    {
+        creditsPanel.SetActive(true);
     }
 
     private void ExitGame()
@@ -37,5 +54,10 @@ public class TitleUIManager : MonoBehaviour
         #else
 	    Application.Quit();
         #endif
+    }
+
+    private void ClosePanel()
+    {
+        creditsPanel.SetActive(false);
     }
 }
